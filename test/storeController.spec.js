b238fbeed71aa1cf1faadcff3f7754b785f7e583
katebeavis <katebeavis84@gmail.com>
2015-05-22 14:41:57 +0100

    Feature test for page displaying item names

--- test/storeController.spec.js
+++ test/storeController.spec.js
@@ -10,4 +10,8 @@ describe('storeController', function() {
   it('initialises with a list of items of clothing', function() {
     expect(ctrl.products).toBeDefined();
   });
+
+  it('has item names', function() {
+    expect(ctrl.products[0].name).toEqual('shoe')
+  });
 });
