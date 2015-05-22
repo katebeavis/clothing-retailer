64475d5d02ca2b022d622266443ff30c17e5d0b7
katebeavis <katebeavis84@gmail.com>
2015-05-22 14:40:53 +0100

    Added unit test for page displaying list of clothing

--- test/e2e/clothingRetailerFeature.js
+++ test/e2e/clothingRetailerFeature.js
@@ -4,4 +4,9 @@ describe('Store', function() {
     expect(browser.getTitle()).toEqual('Clothing Retailer');
   });
 
+  it ('displays items of clothings', function() {
+    browser.get('http://localhost:3000')
+    expect(element(by.id('clothing')).isDisplayed()).toBe(true);
+  });
+
 });
